270d2829d7b2088815afa68fbcb43ab90dc09cfb
ecnivtwelve <vincelinise@gmail.com>
2020-04-29 17:49:07 +0000

    vendor: Add our bootanimation

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -11,4 +11,4 @@
 # limitations under the License.
 
 PRODUCT_COPY_FILES += \
-    vendor/fluid/prebuilt/common/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+    vendor/aosuip/prebuilt/common/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
