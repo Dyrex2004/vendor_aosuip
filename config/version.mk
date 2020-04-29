7f0413dedb975ff76623c1abafb36d1a19c744ab
ender1324 <ender1324@abv.bg>
2020-04-29 17:42:01 +0000

    vendor: version: Bringup our versioning

--- config/version.mk
+++ config/version.mk
@@ -13,39 +13,38 @@
 # limitations under the License.
 
 # Versioning System
-FLUID_CODENAME = Quenol
-FLUID_NUM_VERSION = ZeroTwo
+AOSUIP_NUM_VERSION = 3.0
 
-TARGET_PRODUCT_SHORT := $(subst FluidOS_,,$(FLUID_BUILD_TYPE))
+TARGET_PRODUCT_SHORT := $(subst AOSuiP_,,$(AOSUIP_BUILD_TYPE))
 
-ifndef FLUID_BUILD_TYPE
-    FLUID_BUILD_TYPE := UNOFFICIAL
+ifndef AOSUIP_BUILD_TYPE
+    AOSUIP_BUILD_TYPE := UNOFFICIAL
 endif
 
 # Only include Updater for official, weeklies, and nightly builds
-ifeq ($(filter-out OFFICIAL WEEKLIES NIGHTLY,$(FLUID_BUILD_TYPE)),)
+ifeq ($(filter-out OFFICIAL WEEKLIES NIGHTLY,$(AOSUIP_BUILD_TYPE)),)
     PRODUCT_PACKAGES += \
         Updater
 endif
 
 # Sign builds if building an official, weekly and nightly build
-ifeq ($(filter-out OFFICIAL WEEKLIES NIGHTLY,$(FLUID_BUILD_TYPE)),)
+ifeq ($(filter-out OFFICIAL WEEKLIES NIGHTLY,$(AOSUIP_BUILD_TYPE)),)
     PRODUCT_DEFAULT_DEV_CERTIFICATE := $(KEYS_LOCATION)
 endif
 
 # Set all versions
 BUILD_DATE := $(shell date -u +%Y%m%d)
 BUILD_TIME := $(shell date -u +%H%M)
-FLUID_BUILD_VERSION := $(FLUID_NUM_VERSION)-$(FLUID_CODENAME)
-FLUID_DISPLAY_VERSION := $(FLUID_BUILD_VERSION)-$(FLUID_BUILD_TYPE)-$(FLUID_BUILD)-$(BUILD_DATE)
-ROM_FINGERPRINT := Fluid/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(BUILD_TIME)
-FLUID_VERSION := $(FLUID_DISPLAY_VERSION)
+AOSUIP_BUILD_VERSION := v$(AOSUIP_NUM_VERSION)
+AOSUIP_DISPLAY_VERSION := $(AOSUIP_BUILD_VERSION)-$(AOSUIP_BUILD)-$(BUILD_DATE)-$(AOSUIP_BUILD_TYPE)
+ROM_FINGERPRINT := aosuip/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(BUILD_TIME)
+AOSUIP_VERSION := $(AOSUIP_DISPLAY_VERSION)
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-  ro.fluid.build.version=$(FLUID_BUILD_VERSION) \
+  ro.fluid.build.version=$(AOSUIP_BUILD_VERSION) \
   ro.fluid.build.date=$(BUILD_DATE) \
-  ro.fluid.buildtype=$(FLUID_BUILD_TYPE) \
+  ro.fluid.buildtype=$(AOSUIP_BUILD_TYPE) \
   ro.fluid.fingerprint=$(ROM_FINGERPRINT) \
-  ro.fluid.version=$(FLUID_VERSION) \
-  ro.fluid.device=$(FLUID_BUILD) \
-  ro.modversion=$(FLUID_VERSION)
+  ro.aosuip.version=$(FLUID_VERSION) \
+  ro.fluid.device=$(AOSUIP_BUILD) \
+  ro.modversion=$(AOSUIP_VERSION)
